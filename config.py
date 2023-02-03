eee6a2201fb3f03987eb36902c1d2222b3aedd4e
Johannes Leander <johleander@gmail.com>
2023-02-03 16:48:29 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://johleander-db.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'CegvJVmJlTJym9R1l802bUxFLFirPItKX3OeiR5zgefywYPQu5V3QgzGeF1UF9W1Ezfpp0mLYdHXfDra9OFA2A=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'FootballDB'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'FootballContainer'),
-}
